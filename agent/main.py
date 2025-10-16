ebef51883ecfb6e5606754df2998716e1027cb31
Your Name <user@example.com>
2025-10-16 23:15:36 +0800

    更新项目文件和配置

--- agent/main.py
+++ agent/main.py
@@ -3,8 +3,8 @@ import sys
 from maa.agent.agent_server import AgentServer
 from maa.toolkit import Toolkit
 
-import my_action
-import my_reco
+from . import my_action
+from . import my_reco
 
 
 def main():
